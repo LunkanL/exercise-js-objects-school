36886f4873bac10d746a1145bb19d08d6c36b391
LunkanL <lunkan2002@gmail.com>
2024-11-05 14:37:02 +0100

    fixed methods

--- script.js
+++ script.js
@@ -8,59 +8,118 @@ const school = {
     teachers:[],
 }
 
+// Subjects
+
 const math = {
-    name: "math",
+    name: "Math",
     students: [],
     teacher: {},
+    addTeacher: function (teacher) {
+        teacher = teacher.name;
+        this.teacher.name(teacher);
+    },
+    addStudent: function (student) {
+        student = student.name;
+        this.students.push(student);
+    },
 }
 const coding = {
     name: "Coding",
     students: [],
     teacher: {},
+    addTeacher: function (teacher) {
+        teacher = teacher.name;
+        this.teacher.name(teacher);
+    },
+    addStudent: function (student) {
+        student = student.name;
+        this.students.push(student);
+    },
 }
 const english = {
     name: "English",
     students: [],
     teacher: {},
+    addTeacher: function (teacher) {
+        teacher = teacher.name;
+        this.teacher.name(teacher);
+    },
+    addStudent: function (student) {
+        student = student.name;
+        this.students.push(student);
+    },
 }
+
+// Students
 
 const Niklas = {
     name: "Niklas",
     age: 33,
-    gender: "MAle",
+    gender: "male",
     subjects: [],
+    enlistToSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 const jonas = {
     name: "Jonas",
     age: 54,
     gender: "male",
     subjects: [],
+    enlistToSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 const morgane = {
     name: "Morgane",
     age: 27,
     gender: "female",
     subjects: [],
+    enlistToSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 const ludvig = {
     name: "Ludvig",
     age: 22,
     gender: "male",
     subjects: [],
+    enlistToSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 const ida = {
     name: "Ida",
     age: 20,
     gender: "female",
     subjects: [],
+    enlistToSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
+
+// Teachers
+
 const jessica = {
     name: "Jessica",
     subjects: [],
+    addSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 const anita = {
     name: "Anita",
     subjects: [],
+    addSubject: function (subject) {
+        subject = subject.name;
+        this.subjects.push(subject);
+    },
 }
 
 // 5
@@ -71,9 +130,9 @@ console.log(jessica);
 
 // 6
 
-math.students.push(ida);
+// math.students.push(ida);
 
-console.log(math);
+// console.log(math);
 
 // 7
 
@@ -83,23 +142,21 @@ const addSubjectToTeacher = function (teacher, subject) {
     return teacher;
 }
 
-const addTeacher = function (teacher) {
+// 8
+
+/* addTeacher: function (teacher) {
+    teacher = teacher.name;
+    this.teacher.name(teacher);
+},
+enlistToSubject: function (subject) {
     subject = subject.name;
-    teacher.subjects.push(subject);
-    return teacher;
-}
-const enlistToSubject = function (subject) {
+    this.subjects.push(subject);
+},
+addStudent: function (student) {
+    student = student.name;
+    this.students.push(student);
+},
+addSubject: function (subject) {
     subject = subject.name;
-    teacher.subjects.push(subject);
-    return teacher;
-}
-const addStudent = function (student) {
-    subject = subject.name;
-    teacher.subjects.push(subject);
-    return teacher;
-}
-const addSubject = function (subject) {
-    subject = subject.name;
-    teacher.subjects.push(subject);
-    return teacher;
-}
+    this.subjects.push(subject);
+}, */
